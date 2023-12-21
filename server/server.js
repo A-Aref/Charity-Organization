42886c82d6c49ab5b94e49baaa492e3ccb5ea037
MohanadElmahalawy <mohanadtariq12@gmail.com>
2023-12-21 06:41:57 +0300

    modified:   client/Charity_Org/src/Donor/Account.jsx 	modified:   client/Charity_Org/src/Donor/Donations.jsx 	modified:   client/Charity_Org/src/Donor/Donor.jsx 	modified:   client/Charity_Org/src/Donor/Events.jsx 	modified:   client/Charity_Org/src/Register.jsx 	modified:   client/Charity_Org/src/Routing.jsx 	modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ const con = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "1234",
+
   database: "lab1"
 });
 
@@ -24,7 +25,7 @@ const mysql = require('mysql');
 const con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "1234",
+  password: "0504217246T1s2M3m4M5",
   database: "charity_org1"
 });
 
@@ -59,7 +60,7 @@ app.post("/api/signin", (req,res)=>{
       }
       return res.json(JSON.stringify(result[0]))
     });
-})
+}) 
 
 app.post("/api/leader/updateAccount", (req,res)=>{
   con.query('Update volunteers set FName = ? , LName = ? , Email = ? , Phone = ? , Pass = ? where V_ID = ?' ,[req.body.FName,req.body.LName,req.body.Email,req.body.Phone,req.body.Pass,req.body.V_ID], function (err, result) {
@@ -241,6 +242,59 @@ app.post("/api/volunteer/getParticipations", (req,res)=>{
     }
     return res.json(JSON.stringify(result))
   });
-})
+}) 
 
-var listener = app.listen(5000,()=>{console.log(listener.address().port)})
+app.post("/api/Donor/createmoneydonation", (req,res)=>{
+  con.query('INSERT INTO moneydonations (D_Date,Delivery,Purpose,Currency,Amount) VALUES (?, ?, ?, ?,?)' ,[req.body.D_Date,req.body.Delivery,req.body.Purpose,req.body.Currency,req.body.Amount], function (err, result) {
+    console.log("hello")
+    if (err) throw err 
+    if (result[0] === undefined)
+    {
+      console.log(err)
+      return res.json("No money donations")
+    }
+    return res.json(JSON.stringify(result))
+  });  
+}) 
+app.post("/api/Donor/createclothesdonation", (req,res)=>{
+  con.query('INSERT INTO clothes (D_Date,Delivery,Size,Quality,C_Type,Quantity) VALUES (?, ?, ?, ?,?,?)' ,[req.body.D_Date,req.body.Delivery,req.body.Size,req.body.Quality,req.body.C_Type,req.body.Capacity], function (err, result) {
+    console.log("aywaan")
+    if (err) throw err 
+    if (result[0] === undefined) 
+    {
+      console.log(err)
+      return res.json("No Clothes donations")
+    }
+    return res.json(JSON.stringify(result)) 
+  });  
+}) 
+
+app.post("/api/Donor/creategeneraldonation", (req,res)=>{
+  con.query('INSERT INTO generaldonations(D_Date,Delivery,Descrip,Quantity) VALUES (?, ?, ?, ?)' ,[req.body.D_Date,req.body.Delivery,req.body.Descrip,req.body.Quantity], function (err, result) {
+    console.log("aywaan3")
+    if (err) throw err 
+    if (result[0] === undefined) 
+    {
+      console.log(err)
+      return res.json("No general donations")
+    }
+    return res.json(JSON.stringify(result)) 
+  });  
+}) 
+
+app.post("/api/Register/createRegestration", (req,res)=>{
+  con.query('INSERT INTO donors(Phone,Email,Address,Fname,Lname,Pass) VALUES (?, ?, ?, ?,?,?)' ,[req.body.Phone,req.body.Email,req.body.Address,req.body.Fname,req.body.Lname,req.body.Password], function (err, result) {
+    console.log("aywaan registrationnn")
+    if (err) throw err 
+    if (result[0] === undefined) 
+    {
+      console.log(err)
+      return res.json("No donors")
+    }
+    return res.json(JSON.stringify(result)) 
+  });  
+}) 
+
+
+
+var listener = app.listen(5000,()=>{console.log(listener.address().port)}) 
